d05c5b9d30c87f1d5661e90dd31f5c791d92892b
Hoang Pham <hoanghuypham2121@gmail.com>
2022-05-31 15:49:44 -0400

    initial commit

---
+++ pages/api/socket.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Server } from "Socket.IO";
+import { Socket } from "socket.io/dist/socket";
+
+const SocketHandler = (req, res) => {
+  if (res.socket.server.io) {
+    console.log("Socket is already running");
+    // console.log(res.socket.server.io);
+  } else {
+    console.log("Socket is initializing");
+    const io = new Server(res.socket.server);
+    io.on("connection", async (socket) => {
+      io.emit("user-join", Array.from(await io.allSockets()));
+
+      socket.on("click", () => {
+        io.emit("handle-click", "fuck");
+      });
+
+      socket.on("disconnect", () => {
+        socket.broadcast.emit("user-leave", socket.id);
+      });
+    });
+
+    res.socket.server.io = io;
+  }
+  res.end();
+};
+
+export default SocketHandler;
