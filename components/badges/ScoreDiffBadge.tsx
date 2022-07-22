0960fa74b86d83c1f08c309100d899fa748574ec
Hoang Pham <hoanghuypham2121@gmail.com>
2022-07-22 17:54:56 -0400

    Refactor UI + Logic

---
+++ components/badges/ScoreDiffBadge.tsx
@@ -0,0 +1,21 @@
+import { BsCaretDownFill, BsCaretUpFill } from "react-icons/bs";
+
+interface ScoreDiffBadgeProps {
+  scoreDiff: number;
+  className?: string;
+}
+const ScoreDiffBadge = ({ scoreDiff, className }: ScoreDiffBadgeProps) => {
+  return scoreDiff < 0 ? (
+    <div className={`flex w-fit items-center space-x-1 rounded-lg ${className}`}>
+      <BsCaretDownFill size={15} className="fill-red-400" />
+      <h3>{Math.abs(scoreDiff)}</h3>
+    </div>
+  ) : scoreDiff > 0 ? (
+    <div className={`flex w-fit items-center space-x-1 rounded-lg ${className}`}>
+      <BsCaretUpFill size={15} className="fill-green-400" />
+      <h3>{Math.abs(scoreDiff)}</h3>
+    </div>
+  ) : null;
+};
+
+export default ScoreDiffBadge;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  },
 };
 
 module.exports = nextConfig;
